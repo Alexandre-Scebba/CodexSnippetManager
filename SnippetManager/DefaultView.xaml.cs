f3495f1577c1bd752309e64ecffefb3b14260e88
vvaraksn <dimitriteolis21@gmail.com>
2024-11-23 15:53:15 -0500

    login register

--- SnippetManager/DefaultView.xaml.cs
+++ SnippetManager/DefaultView.xaml.cs
@@ -1,24 +1,8 @@
-﻿using SnippetManager.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SnippetManager
 {
-    /// <summary>
-    /// Interaction logic for DefaultView.xaml
-    /// </summary>
     public partial class DefaultView : UserControl
     {
         public DefaultView()
@@ -26,20 +10,11 @@ namespace SnippetManager
             InitializeComponent();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            if (DataContext is MainViewModel viewModel)
+            if (DataContext is ViewModels.MainViewModel viewModel)
             {
-                // Replace with actual logic for navigating to login view (if any)
-                MessageBox.Show("Login functionality not implemented.");
-            }
-        }
-
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (DataContext is MainViewModel viewModel)
-            {
-                viewModel.ShowRegistrationView();
+                viewModel.Password = ((PasswordBox)sender).Password;
             }
         }
     }
