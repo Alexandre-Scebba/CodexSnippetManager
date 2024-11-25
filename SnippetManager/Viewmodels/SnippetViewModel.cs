e076e51eb07399f73db58572ef3cbc88d596d1fc
vvaraksn <dimitriteolis21@gmail.com>
2024-11-25 16:18:20 -0500

    import export using DTOS going to fix select all soon

---
+++ SnippetManager/Viewmodels/SnippetViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using SnippetManager.Models;
+using System.Diagnostics;
+
+namespace SnippetManager.ViewModels
+{
+    public class SnippetViewModel : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+
+        public Snippet Snippet { get; set; }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                    Debug.WriteLine($"Snippet '{Snippet.Title}' selection changed to: {_isSelected}");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
